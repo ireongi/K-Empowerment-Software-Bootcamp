577e9befce8ad6292db7e2af2c8576f9254483ba
ireongi <ireongi@gmail.com>
2023-02-09 23:11:48 +0900

    .

--- 09_Graph/p334.py
+++ 09_Graph/p334.py
@@ -9,18 +9,18 @@ class Graph():
 G1 = None
 stack = []
 visited_array = []  # 방문한 정점
-name_array = ['문별', '솔라', '휘인', '쯔위', '선미', '화사']
-문별, 솔라, 휘인, 쯔위, 선미, 화사 = 0, 1, 2, 3, 4, 5
+name_array = ['김', '이', '박', '최', '강', '조']
+김, 이, 박, 최, 강, 조 = 0, 1, 2, 3, 4, 5
 
 
 ## 메인 코드 부분 ##
 G1 = Graph(6)
-G1.graph[문별][솔라] = 1; G1.graph[문별][휘인] = 1
-G1.graph[솔라][문별] = 1; G1.graph[솔라][쯔위] = 1
-G1.graph[휘인][문별] = 1; G1.graph[휘인][쯔위] = 1
-G1.graph[쯔위][솔라] = 1; G1.graph[쯔위][휘인] = 1; G1.graph[쯔위][선미] = 1; G1.graph[쯔위][화사] = 1
-G1.graph[선미][쯔위] = 1; G1.graph[선미][화사] = 1
-G1.graph[화사][쯔위] = 1; G1.graph[화사][선미] = 1
+G1.graph[김][이] = 1; G1.graph[김][박] = 1
+G1.graph[이][김] = 1; G1.graph[이][최] = 1
+G1.graph[박][김] = 1; G1.graph[박][최] = 1
+G1.graph[최][이] = 1; G1.graph[최][박] = 1; G1.graph[최][강] = 1; G1.graph[최][조] = 1
+G1.graph[강][최] = 1; G1.graph[강][조] = 1
+G1.graph[조][최] = 1; G1.graph[조][강] = 1
 
 print('## G1 무방향 그래프 ##')
 for row in range(6):
