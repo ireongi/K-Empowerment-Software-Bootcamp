664f2e92c20f69be1147ec5b151f7af3fda1d2c0
ireongi <ireongi@gmail.com>
2023-02-27 22:58:00 +0900

    .

--- DataBase/Example.sql
+++ DataBase/Example.sql
@@ -10,21 +10,18 @@ primary key(id) )
 engine = innoDB default character set = utf8;
 
 insert into student(name, dept, id) values('홍길동', '체육학과', '1111111');
-
-select * from student;
-
 insert into student(name, dept, id) values('최고봉', '멀티미디어', '0792012');
-
 insert into student(name, dept, id) values('이기자', '경제학과', '1233211');
 
+update student set dept = '경영학과' where id = '0792012';
 select * from student;
-
-update student set dept = '건축공학' where id = '0792012';
 
 delete from student where name = '최고봉';
-select * from student;
-
 delete from student where id = '1111111';
+
+select * from student;
+delete from student where name = '유재석';
+select * from student;
 
 commit;
 
